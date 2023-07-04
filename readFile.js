873405eb4b09facff2748c46aeb1b0f832fc99b4
yotamgra <yotam.granada@gmail.com>
2023-07-04 14:47:44 +0300

    BUG - readFile added from chatGPT but it crashes

---
+++ readFile.js
@@ -0,0 +1,80 @@
+const fs = require('fs');
+
+function parseVCFFile(filename) {
+  const variants = [];
+  const fileData = fs.readFileSync(filename, 'utf8');
+  const lines = fileData.split('\n');
+
+  let headers = [];
+  lines.forEach((line) => {
+    line = line.trim();
+    if (line.startsWith('##')) {
+      // Skip header lines
+      return;
+    } else if (line.startsWith('#')) {
+      // Parse column headers
+      headers = line.split('\t').slice(9);
+      return;
+    }
+
+    const fields = line.split('\t');
+    const chrom = fields[0];
+    const pos = parseInt(fields[1]);
+    const ref = fields[3];
+    const alt = fields[4];
+    const infoItems = fields[7].split(';');
+    const info = {};
+    infoItems.forEach((item) => {
+      const [key, value] = item.split('=');
+      info[key] = value;
+    });
+    const formatFields = fields[8].split(':');
+    const samples = fields.slice(9);
+
+    const variant = {
+      chromosome: chrom,
+      position: pos,
+      refAllele: ref,
+      altAllele: alt,
+      info,
+      formatFields,
+      samples: [],
+    };
+
+    samples.forEach((sample, i) => {
+      const sampleFields = sample.split(':');
+      const sampleData = {};
+      formatFields.forEach((field, j) => {
+        sampleData[field] = sampleFields[j];
+      });
+      variant.samples.push({
+        sampleId: headers[i],
+        data: sampleData,
+      });
+    });
+
+    variants.push(variant);
+  });
+
+  return variants;
+}
+
+// Usage example
+const filename = 'output.vcf';
+const variants = parseVCFFile(filename);
+
+// Accessing variant information
+variants.forEach((variant) => {
+  console.log(`Chromosome: ${variant.chromosome}`);
+  console.log(`Position: ${variant.position}`);
+  console.log(`Ref Allele: ${variant.refAllele}`);
+  console.log(`Alt Allele: ${variant.altAllele}`);
+  console.log('INFO:', variant.info);
+  console.log('Format Fields:', variant.formatFields);
+  console.log('Samples:');
+  variant.samples.forEach((sample) => {
+    console.log(`- Sample ID: ${sample.sampleId}`);
+    console.log('  Data:', sample.data);
+  });
+  console.log('---');
+});
