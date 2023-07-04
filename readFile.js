50652b3da06986a41e072cc61a3da1448b38855b
yotamgra <yotam.granada@gmail.com>
2023-07-04 14:54:34 +0300

    FIX - ignore empty lines

--- readFile.js
+++ readFile.js
@@ -1,36 +1,39 @@
-const fs = require('fs');
+const fs = require("fs");
 
 function parseVCFFile(filename) {
   const variants = [];
-  const fileData = fs.readFileSync(filename, 'utf8');
-  const lines = fileData.split('\n');
+  const fileData = fs.readFileSync(filename, "utf8");
+  const lines = fileData.split("\n");
 
   let headers = [];
   lines.forEach((line) => {
     line = line.trim();
-    if (line.startsWith('##')) {
+    if (!line || line.startsWith("##")) {
       // Skip header lines
       return;
-    } else if (line.startsWith('#')) {
+    } else if (line.startsWith("#")) {
       // Parse column headers
-      headers = line.split('\t');
+      headers = line.split("\t");
       headers[0] = headers[0].substring(1);
-      console.log(headers)
+      console.log(headers);
       return;
     }
 
-    const fields = line.split('\t');
+    const fields = line.split("\t");
     const chrom = fields[0];
     const pos = parseInt(fields[1]);
     const ref = fields[3];
     const alt = fields[4];
-    const infoItems = fields[7].split(';');
+    if (!fields[7]) {
+      console.log("line", line);
+    }
+    const infoItems = fields[7].split(";");
     const info = {};
     infoItems.forEach((item) => {
-      const [key, value] = item.split('=');
+      const [key, value] = item.split("=");
       info[key] = value;
     });
-    const formatFields = fields[8].split(':');
+    const formatFields = fields[8].split(":");
     const samples = fields.slice(9);
 
     const variant = {
@@ -44,7 +47,7 @@ function parseVCFFile(filename) {
     };
 
     samples.forEach((sample, i) => {
-      const sampleFields = sample.split(':');
+      const sampleFields = sample.split(":");
       const sampleData = {};
       formatFields.forEach((field, j) => {
         sampleData[field] = sampleFields[j];
@@ -62,7 +65,7 @@ function parseVCFFile(filename) {
 }
 
 // Usage example
-const filename = 'output.vcf';
+const filename = "output.vcf";
 const variants = parseVCFFile(filename);
 
 // Accessing variant information
@@ -71,12 +74,12 @@ variants.forEach((variant) => {
   console.log(`Position: ${variant.position}`);
   console.log(`Ref Allele: ${variant.refAllele}`);
   console.log(`Alt Allele: ${variant.altAllele}`);
-  console.log('INFO:', variant.info);
-  console.log('Format Fields:', variant.formatFields);
-  console.log('Samples:');
+  console.log("INFO:", variant.info);
+  console.log("Format Fields:", variant.formatFields);
+  console.log("Samples:");
   variant.samples.forEach((sample) => {
     console.log(`- Sample ID: ${sample.sampleId}`);
-    console.log('  Data:', sample.data);
+    console.log("  Data:", sample.data);
   });
-  console.log('---');
+  console.log("---");
 });
