182881b30baec991e01b55e3aac98d94200caef5
yotamgra <yotam.granada@gmail.com>
2023-07-05 19:11:04 +0300

    download route created

--- src/services/download&convert.js
+++ src/services/download&convert.js
@@ -1,54 +1,50 @@
-import axios from "axios";
-import fs from "fs";
-import { createGunzip } from "zlib";
+import axios from 'axios';
+import fs from 'fs';
+import zlib from 'zlib';
 
-const url =
-  "https://s3.amazonaws.com/resources.genoox.com/homeAssingment/demo_vcf_multisample.vcf.gz";
-const downloadFolderPath = "src/downloads/";
-const gzippedFile = downloadFolderPath + "output.vcf.gz";
-const outputFile = downloadFolderPath + "output.vcf";
+const url = 'https://s3.amazonaws.com/resources.genoox.com/homeAssingment/demo_vcf_multisample.vcf.gz';
+const downloadFolderPath = 'src/downloads/'; // Specify the folder path here
+const gzippedFile = downloadFolderPath + 'output.vcf.gz';
+const outputFile = downloadFolderPath + 'output.vcf';
 
-export const downloadFile = async () => {
+async function downloadFile() {
   try {
     const response = await axios({
-      method: "get",
+      method: 'get',
       url: url,
-      responseType: "stream",
+      responseType: 'stream',
     });
 
     const fileStream = fs.createWriteStream(gzippedFile);
     response.data.pipe(fileStream);
 
     return new Promise((resolve, reject) => {
-      fileStream.on("finish", resolve);
-      fileStream.on("error", reject);
+      fileStream.on('finish', resolve);
+      fileStream.on('error', reject);
     });
   } catch (error) {
-    throw new Error("Error downloading the file:", error);
-  }
-};
-
-export async function convertFileToText() {
-  try {
-    await downloadFile();
-    const gunzipPromise = new Promise((resolve, reject) => {
-      const gunzip = createGunzip();
-      const inputStream = fs.createReadStream(gzippedFile);
-      const outputStream = fs.createWriteStream(outputFile);
-      
-      inputStream.pipe(gunzip).pipe(outputStream);
-
-      inputStream.on("end", resolve);
-      gunzip.on("error", reject);
-      outputStream.on("error", reject);
-    });
-
-    await gunzipPromise;
-
-    console.log("File converted to text successfully.");
-  } catch (error) {
-    console.error("An error occurred:", error);
+    throw new Error('Error downloading the file:', error);
   }
 }
 
-convertFileToText();
+export const convertFileToText= async()=> {
+  try {
+    await downloadFile();
+
+    const inputStream = fs.createReadStream(gzippedFile);
+    const outputStream = fs.createWriteStream(outputFile);
+
+    await new Promise((resolve, reject) => {
+      inputStream
+        .pipe(zlib.createGunzip())
+        .pipe(outputStream)
+        .on('finish', resolve)
+        .on('error', reject);
+    });
+
+    console.log('File converted to text successfully.');
+  } catch (error) {
+    console.error('An error occurred:', error);
+  }
+}
+
