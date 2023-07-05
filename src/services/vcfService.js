006b5ce9a976a4d05a32f4595f9b7323e65311ad
yotamgra <yotam.granada@gmail.com>
2023-07-05 15:06:14 +0300

    add fetchVariantDetails function and add to the file gene info

--- src/services/vcfService.js
+++ src/services/vcfService.js
@@ -1,7 +1,8 @@
 import fs from "fs";
 import { createGunzip } from "zlib";
+import axios from "axios";
 
-export const parseVCFFile = (filename, start, end, minDP, limit) => {
+export const parseVCFFile = async (filename, start, end, minDP, limit) => {
   // Create the three new files: father_filtered.vcf, mother_filtered.vcf, proband_filtered.vcf
   const fileNames = [
     "src/files/father_filtered.vcf",
@@ -53,17 +54,20 @@ export const parseVCFFile = (filename, start, end, minDP, limit) => {
     } else {
       // Process data line
       const fields = line.split("\t");
-      const chrom = fields[0];
+      const chr = fields[0];
       const pos = parseInt(fields[1]);
+      const id = fields[2];
       const ref = fields[3];
       const alt = fields[4];
+      const qual = fields[5];
+      const filter = fields[6];
       const infoItems = fields[7].split(";");
       const info = {};
       infoItems.forEach((item) => {
         const [key, value] = item.split("=");
         info[key] = value;
       });
-      const formatFields = fields[8].split(":");
+      const formatFields = fields[8];
 
       if (start && pos >= start) {
         if (end && pos <= end) {
@@ -78,20 +82,37 @@ export const parseVCFFile = (filename, start, end, minDP, limit) => {
             //check if the sample data contains numbers if so it exists else it doesnt
             if (sampleFather.match(/\d+/g)) {
               if (fatherVarientCount < limit) {
-                //push the fieldes in index 0-8 and add the sample data in index 9
-                fs.appendFileSync(fileNames[0], `${line}\t${sampleFather}\n`);
+                const gene = await fetchVariantDetails({ chr, pos, ref, alt });
+                //add gene to info field name GENE and add the gene name
+                const newInfo = `${fields[7]};GENE=${gene}`;
+
+                //push the fieldes to the file
+                fs.appendFileSync(
+                  fileNames[0],
+                  `${chr}\t${pos}\t${id}\t${ref}\t${alt}\t${qual}\t${filter}\t${newInfo}\t${formatFields}\t${sampleFather}\n`
+                );
                 fatherVarientCount++;
               }
             }
             if (sampleMother.match(/\d+/g)) {
               if (motherVarientCount < limit) {
-                fs.appendFileSync(fileNames[1], `${line}\t${sampleMother}\n`);
+                const gene = await fetchVariantDetails({ chr, pos, ref, alt });
+                const newInfo = `${fields[7]};GENE=${gene}`;
+                fs.appendFileSync(
+                  fileNames[1],
+                  `${chr}\t${pos}\t${id}\t${ref}\t${alt}\t${qual}\t${filter}\t${newInfo}\t${formatFields}\t${sampleFather}\n`
+                );
                 motherVarientCount++;
               }
             }
             if (sampleProband.match(/\d+/g)) {
               if (probandVarientCount < limit) {
-                fs.appendFileSync(fileNames[2], `${line}\t${sampleProband}\n`);
+                const gene = await fetchVariantDetails({ chr, pos, ref, alt });
+                const newInfo = `${fields[7]};GENE=${gene}`;
+                fs.appendFileSync(
+                  fileNames[2],
+                  `${chr}\t${pos}\t${id}\t${ref}\t${alt}\t${qual}\t${filter}\t${newInfo}\t${formatFields}\t${sampleFather}\n`
+                );
                 probandVarientCount++;
               }
             }
@@ -102,4 +123,21 @@ export const parseVCFFile = (filename, start, end, minDP, limit) => {
   }
 
   return "success";
+};
+
+const fetchVariantDetails = async ({ chr, pos, ref, alt }) => {
+  const VariantDetailsURL =
+    "https://franklin.genoox.com/api/fetch_variant_details";
+  const response = await axios({
+    method: "post",
+    url: VariantDetailsURL,
+    data: {
+      chr: chr,
+      pos: pos,
+      ref: ref,
+      alt: alt,
+      reference_version: "hg19",
+    },
+  });
+  return response.data.gene;
 };
