d723f86e7d68898fbb2d6cae526f2ad74f5c35af
yotamgra <yotam.granada@gmail.com>
2023-07-06 16:03:54 +0300

    use the streamed data instead the downloaded file

--- src/routes/vcfRoutes.js
+++ src/routes/vcfRoutes.js
@@ -2,15 +2,11 @@ import express from "express";
 import { processVcf } from "../controllers/vcfController.js";
 import { downloadAndConvert } from "../controllers/vcfController.js";
 import { streamingVcf } from "../services/streamingVcf.js";
-import { lines } from "../services/streamingVcf.js";
 
 const router = express.Router();
 
 router.route("/").post(processVcf);
-router.route("/test").get((req, res) => {
-    console.log(lines);
-  res.send("test");
-});
 router.route("/streaming").get(streamingVcf);
 router.route("/download").get(downloadAndConvert);
+
 export default router;
